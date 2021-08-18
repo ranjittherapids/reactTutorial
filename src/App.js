c4faca1b1b3b3967d6368214d4f6d38dd3d79795
Ranjit kumar sah <85950577+ranjittherapids@users.noreply.github.com>
2021-08-18 18:05:14 +0530

    o

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>hi</h2>
+      <h1>hi</h1>
     </div>
   );
 }
